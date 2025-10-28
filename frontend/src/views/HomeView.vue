08a3b6c5adcbb9037d89d5e6f6c0d6daafcf33a5
Mattz <matias.marti@medieinstitutet.com>
2025-10-28 16:18:02 +0100

    remove comma

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,7 +6,7 @@ import Logo from '@/components/Logo.vue';
 <template>
   <main>
     <h1 class="text-8xl text-center font-bold pt-10 w-full uppercase">
-      Get <br /> Stress-free, <br /> LP Yield
+      Get <br /> Stress-free<br /> LP Yield
       <Logo class="inline-block pb-2 ml-4" />
     </h1>
     <p class="text-center mb-8 text-2xl">
