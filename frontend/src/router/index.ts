3bc78d7c646c4fb5bd7263020b31ac722c03f1f3
Mattz <matias.marti@medieinstitutet.com>
2025-11-06 16:35:26 +0100

    Update pool details route to use consistent path '/pools/:id'

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -31,7 +31,7 @@ const router = createRouter({
 			component: () => import('../views/LiquidityPoolsView.vue'),
 		},
 		{
-			path: '/pool/:id',
+			path: '/pools/:id',
 			name: 'pool-details',
 			component: () => import('../views/LiquidityPoolDetailsView.vue'),
 		},
